a09c601d44083e337ab3626245b23826ac2b9122
Esity <matthewdiverson@gmail.com>
2026-03-21 23:55:45 -0500

    feat: wire lex-governance review into lifecycle transition

--- lib/legion/digital_worker/lifecycle.rb
+++ lib/legion/digital_worker/lifecycle.rb
@@ -48,6 +48,7 @@ module Legion
       class InvalidTransition  < StandardError; end
       class GovernanceRequired < StandardError; end
       class AuthorityRequired  < StandardError; end
+      class GovernanceBlocked  < StandardError; end
 
       def self.transition!(worker, to_state:, by:, reason: nil, **opts)
         from_state = worker.lifecycle_state
@@ -55,13 +56,24 @@ module Legion
 
         raise InvalidTransition, "cannot transition from #{from_state} to #{to_state}" unless allowed.include?(to_state)
 
-        if governance_required?(from_state, to_state)
-          required = GOVERNANCE_REQUIRED[[from_state, to_state]]
-          raise GovernanceRequired, "#{from_state} -> #{to_state} requires #{required}" unless opts[:governance_override] == true
-        end
+        if defined?(Legion::Extensions::Governance::Runners::Governance)
+          review = Legion::Extensions::Governance::Runners::Governance.review_transition(
+            worker_id:    worker.is_a?(Hash) ? worker[:id] : worker.worker_id,
+            from_state:   from_state,
+            to_state:     to_state,
+            principal_id: by,
+            worker_owner: worker.respond_to?(:owner_msid) ? worker.owner_msid : nil
+          )
+          raise GovernanceBlocked, "#{from_state} -> #{to_state} blocked: #{review[:reasons]&.join(', ')}" unless review[:allowed]
+        else
+          if governance_required?(from_state, to_state)
+            required = GOVERNANCE_REQUIRED[[from_state, to_state]]
+            raise GovernanceRequired, "#{from_state} -> #{to_state} requires #{required}" unless opts[:governance_override] == true
+          end
 
-        authority = authority_type(from_state, to_state)
-        raise AuthorityRequired, "#{from_state} -> #{to_state} requires #{authority} (by: #{by})" if authority && opts[:authority_verified] != true
+          authority = authority_type(from_state, to_state)
+          raise AuthorityRequired, "#{from_state} -> #{to_state} requires #{authority} (by: #{by})" if authority && opts[:authority_verified] != true
+        end
 
         worker.update(
           lifecycle_state: to_state,
