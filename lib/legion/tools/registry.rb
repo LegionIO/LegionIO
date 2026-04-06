088bc3691ebb9b4517f2ce2f8eb669aa6be77a2a
Esity <matthewdiverson@gmail.com>
2026-04-06 16:46:01 -0500

    add always_loaded_names to Tools::Registry

--- lib/legion/tools/registry.rb
+++ lib/legion/tools/registry.rb
@@ -13,21 +13,19 @@ module Legion
           is_deferred = tool_class.respond_to?(:deferred?) && tool_class.deferred?
           bucket = is_deferred ? :deferred : :always
 
+          Legion::Logging.unknown "[Tools::Registry] register called: name=#{name} deferred=#{is_deferred} class=#{tool_class.name || tool_class.inspect}"
+
           @mutex.synchronize do
             target = bucket == :deferred ? @deferred : @always
             other  = bucket == :deferred ? @always : @deferred
 
             if target.any? { |t| t.tool_name == name } || other.any? { |t| t.tool_name == name }
-              if defined?(Legion::Logging)
-                Legion::Logging.warn(
-                  "[Tools::Registry] duplicate registration rejected: #{name} " \
-                  "(attempted by #{tool_class.name || tool_class.inspect})"
-                )
-              end
+              Legion::Logging.unknown "[Tools::Registry] DUPLICATE rejected: #{name}"
               return false
             end
 
             target << tool_class
+            Legion::Logging.unknown "[Tools::Registry] registered: #{name} -> #{bucket} (always=#{@always.size} deferred=#{@deferred.size})"
             true
           end
         end
@@ -51,6 +49,10 @@ module Legion
           end
         end
 
+        def always_loaded_names
+          tools.map(&:tool_name)
+        end
+
         # Catalog queries - replaces Catalog::Registry
         def for_extension(ext_name)
           all_tools.select { |t| t.respond_to?(:extension) && t.extension == ext_name }
@@ -65,6 +67,7 @@ module Legion
         end
 
         def clear
+          Legion::Logging.unknown "[Tools::Registry] clear called (was: always=#{@always.size} deferred=#{@deferred.size})"
           @mutex.synchronize do
             @always.clear
             @deferred.clear
