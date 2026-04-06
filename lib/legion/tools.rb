d658644c78ded3c58358050eb7af932424d6c587
Esity <matthewdiverson@gmail.com>
2026-04-06 14:29:06 -0500

    add Legion::Tools::Base class for canonical tool definitions

---
+++ lib/legion/tools.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Legion
+  module Tools
+    # Static tool classes accumulate here at require time for reload safety
+    @tool_classes = []
+    @mutex = Mutex.new
+
+    class << self
+      def tool_classes
+        @mutex.synchronize { @tool_classes.dup }
+      end
+
+      def register_class(klass)
+        @mutex.synchronize do
+          @tool_classes << klass unless @tool_classes.include?(klass)
+        end
+      end
+
+      # Called by Service#register_core_tools on boot AND reload
+      def register_all
+        @mutex.synchronize { @tool_classes.dup }.each do |klass|
+          Legion::Tools::Registry.register(klass)
+        end
+      end
+    end
+  end
+end
+
+require_relative 'tools/base'
