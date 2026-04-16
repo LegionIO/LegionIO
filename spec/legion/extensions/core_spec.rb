8b22e9249d9f8dc0923971dfa4b49f910df87e0f
Esity <matthewdiverson@gmail.com>
2026-04-15 19:20:36 -0500

    add sticky_tools? to Extensions::Core — defaults true, extensions may override

--- spec/legion/extensions/core_spec.rb
+++ spec/legion/extensions/core_spec.rb
@@ -3,6 +3,23 @@
 require 'spec_helper'
 
 RSpec.describe Legion::Extensions::Core do
+  describe '.sticky_tools?' do
+    it 'returns true by default' do
+      stub_const('Legion::Extensions::StickyTest', Module.new { extend Legion::Extensions::Core })
+      expect(Legion::Extensions::StickyTest.sticky_tools?).to eq(true)
+    end
+
+    it 'can be overridden to false on extension module' do
+      mod = Module.new do
+        extend Legion::Extensions::Core
+        def self.sticky_tools?
+          false
+        end
+      end
+      expect(mod.sticky_tools?).to eq(false)
+    end
+  end
+
   describe '.trigger_words' do
     it 'defaults to lex name segments derived from the module name' do
       stub_const('Legion::Extensions::Github', Module.new { extend Legion::Extensions::Core })
